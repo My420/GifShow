0a2a057bcedc8c8a65d6c8e4332c5524a3d288c8
My420 <My420V@gmail.com>
2019-02-19 14:58:55 +0300

    add style for max-width: 918px

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -5,13 +5,32 @@ import Option from "../Option/Option";
 import LoadBar from "../LoadBar/LoadBar";
 
 class Controls extends Component {
+  state = {
+    isMenuOpen: false
+  };
+
+  changeMenuStatus = () => {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  };
   render() {
     console.log(`render ----- Controls`);
+    const { isMenuOpen } = this.state;
     return (
-      <section className="app__controls">
-        <h2 className="visually-hidden">Управление</h2>
-        <Navigation />
-        <Option />
+      <section className="app__controls controls">
+        <h2 className="visually-hidden">Controls</h2>
+        <div className="controls__toggle-wrapper">
+          <button
+            className={`controls__toggle ${
+              isMenuOpen ? "controls__toggle--close" : "controls__toggle--open"
+            }`}
+            onClick={this.changeMenuStatus}
+          >
+            <span className="visually-hidden">Toggle Menu</span>
+          </button>
+          <p className="controls__text">Menu</p>
+        </div>
+        {isMenuOpen ? <Navigation /> : null}
+        {isMenuOpen ? <Option /> : null}
         <LoadBar />
       </section>
     );
